06106646bcca442ef8846854dd4c41ba42f17fdc
Manuel Quarneti <manuelquarneti@gmail.com>
2022-04-15 14:34:38 +0200

    🚧 Initial news view

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use anyhow::Result;
+
 slint::include_modules!();
 
-fn main() {
+fn main() -> Result<()> {
     let ui = AppWindow::new();
+
+    let news = mc::news::get_minecraft_news(None)?;
+    let titles = news.article_grid.into_iter().map(|article| slint::SharedString::from(article.default_tile.title)).collect::<Vec<slint::SharedString>>();
+    let titles = std::rc::Rc::new(slint::VecModel::from(titles));
+    ui.set_news(titles.clone().into());
+
     ui.run();
+    Ok(())
 }

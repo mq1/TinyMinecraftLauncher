fb2f6f1d2e17091faca7624da01e25ae8d6a621a
Manuel Quarneti <manuelquarneti@gmail.com>
2022-06-01 14:33:49 +0200

    ♻️ Refactoring

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -2,15 +2,16 @@ use druid::{
     widget::{CrossAxisAlignment, Flex, Label},
     Widget,
 };
+use const_format::formatcp;
 
 use crate::AppState;
+
+const APP_VERSION: &str = formatcp!("TinyMinecraftLauncher version {}", env!("CARGO_PKG_VERSION"));
+const LICENSE: &str = "GPLv3 Licensed | © 2022 Manuel Quarneti";
 
 pub fn build_about_widget() -> impl Widget<AppState> {
     Flex::column()
         .cross_axis_alignment(CrossAxisAlignment::Start)
-        .with_child(Label::new(format!(
-            "TinyMinecraftLauncher version {}",
-            env!("CARGO_PKG_VERSION")
-        )))
-        .with_child(Label::new("GPLv3 Licensed | © 2022 Manuel Quarneti"))
+        .with_child(Label::new(APP_VERSION))
+        .with_child(Label::new(LICENSE))
 }

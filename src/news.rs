ad9ede849c810d8f05a8d7a5f25eb9b6eafe2ad6
Manuel Quarneti <manuelquarneti@gmail.com>
2022-06-05 23:20:33 +0200

    ✨ Load news' images

--- src/news.rs
+++ src/news.rs
@@ -1,17 +1,25 @@
 use druid::{
-    widget::{Button, Flex, Label, Scroll},
+    piet::ImageBuf,
+    widget::{Button, Flex, Image, Label, Scroll},
     Widget, WidgetExt,
 };
 
 use crate::AppState;
 
 fn build_item(article: mc::news::Article) -> impl Widget<AppState> {
+    let image_data = attohttpc::get(article.default_tile.image.image_url)
+        .send()
+        .unwrap();
+    let image_buf = ImageBuf::from_data(&image_data.bytes().unwrap()).unwrap();
+    let image = Image::new(image_buf);
+
     let label = Label::new(article.default_tile.title);
 
     let read_button = Button::new("Read")
         .on_click(move |_ctx, _data, _env| open::that(article.article_url.as_str()).unwrap());
 
     Flex::row()
+        .with_child(image)
         .with_child(label)
         .with_flex_spacer(1.)
         .with_child(read_button)
